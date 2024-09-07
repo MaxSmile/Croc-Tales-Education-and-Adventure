2c9a7b1201916a9badc801f29d09951ab631508c
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-09-08 00:04:06 +0930

    crocs

--- croc-tale-web-app/src/pages/CrocStory.jsx
+++ croc-tale-web-app/src/pages/CrocStory.jsx
@@ -11,8 +11,8 @@ const CrocStory = () => {
   const { crocId } = useParams(); // Get the dynamic crocId from the URL
   const crocStory = CrocStoriesData[crocId]; // Get the story based on crocId
 
-  const PlaceholderImage = () => <img src={placeholderImage} alt="Placeholder" 
-  className="w-full max-w-xs rounded-lg shadow-md" />
+  const PlaceholderImage = () => <img src={placeholderImage} alt="Placeholder"
+    className="w-full max-w-xs rounded-lg shadow-md" />
 
   // If crocStory is not found, render the NotFound component
   if (!crocStory) {
@@ -25,8 +25,8 @@ const CrocStory = () => {
         {/* Left side: Image section */}
         <div className="lg:w-1/2 flex justify-center mb-6 lg:mb-0">
           {crocStory.image ? (
-            <img src={crocStory.image} alt={crocStory.title} 
-                className="w-full max-w-xs rounded-lg shadow-md" />
+            <img src={crocStory.image} alt={crocStory.title}
+              className="w-full max-w-xs rounded-lg shadow-md" />
           ) : (
             <PlaceholderImage />
           )}
@@ -39,10 +39,11 @@ const CrocStory = () => {
           <p className="text-md mb-4 text-gray-500">Location: {crocStory.location}</p>
           <p className="text-md mb-4 text-gray-500">Design: {crocStory.design}</p>
           <p className="text-md text-green-600 font-semibold">Crocwise Lesson: {crocStory.crocwiseLesson}</p>
-        </div>
-        <p className="mt-6 lg:mt-0 text-gray-500 text-left">
+          <p className="mt-6 pt-6 lg:mt-0 text-gray-500 text-left">
             <a href="/stories" className="btn-orange">Back to more croc stories</a>
-        </p>
+          </p>
+        </div>
+
       </div>
     </Layout>
   );
